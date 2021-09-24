56846c23d7e9bb165e48a3423cbaf5f84d75d7d6
Jacob Guo <jguo30@stuy.edu>
2021-09-24 00:06:02 -0400

    Finished FtoC function and added FtoC tests

--- CtoFTester.java
+++ CtoFTester.java
@@ -7,14 +7,17 @@ The functions should return the doubles that are the converted temperatures.
 public class CtoFTester{
 
   public static double celsiusToFahrenheit(int celsius){
-    return (celsius * 9.0 / 5.0) + 32.0;
+    return (celsius * 9.0 / 5.0) + 32;
   }
-  //public static double fahrenheitToCelsius(int farenheit){
 
-  //}
+  public static double fahrenheitToCelsius(int farenheit){
+    return (farenheit - 32) * 5.0 / 9.0;
+  }
 
   public static void main(String[] args){
       System.out.println(celsiusToFahrenheit(0));
       System.out.println(celsiusToFahrenheit(50));
+      System.out.println(fahrenheitToCelsius(0));
+      System.out.println(fahrenheitToCelsius(50));
   }
 }
