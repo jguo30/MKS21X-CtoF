c80354a415543b29ea7cabcce58524af5ddb7fd6
Jacob Guo <jguo30@stuy.edu>
2021-09-23 23:42:37 -0400

    Added the class, comments on top, and function shells

---
+++ CtoFTester.java
@@ -0,0 +1,15 @@
+/* These functions should have eithers ints or doubles as the parameters.
+The parameters themselves should be what we are converting, so either Farenheit or Celsius.
+
+The functions should return the doubles that are the converted temperatures.
+
+*/
+public class CtoFTester.java{
+
+  public static double celsiusToFahrenheit(){
+    
+  }
+  public static double fahrenheitToCelsius(){
+
+  }
+}
