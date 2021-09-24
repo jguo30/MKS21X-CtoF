39ff54dbf725c437d8a3361436f0a5019cab9bb2
Jacob Guo <jguo30@stuy.edu>
2021-09-23 23:47:07 -0400

    Added equation and return statement to Celsius to Farenheit

--- CtoFTester.java
+++ CtoFTester.java
@@ -7,7 +7,7 @@ The functions should return the doubles that are the converted temperatures.
 public class CtoFTester.java{
 
   public static double celsiusToFahrenheit(int celsius){
-    
+    return ((celsius * 9.0 / 5.0) + 32.0);
   }
   public static double fahrenheitToCelsius(int farenheit){
 
